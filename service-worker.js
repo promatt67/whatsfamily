6f6459962f565b3fe5f07bd76af8206e8e0ab894
promatt67 <58940514+promatt67@users.noreply.github.com>
2026-07-26 12:31:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ const db = firebase.firestore();
 const messaging = firebase.messaging();
 
 // Cache statici (incrementa il numero di versione ad ogni modifica importante)
-const CACHE_NAME = 'whatsfamily-v5.2';
+const CACHE_NAME = 'whatsfamily-v5.3.1';
 const urlsToCache = [
   './',
   './index.html',
